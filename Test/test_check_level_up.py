e2ff61b8a8e9b9a75104f2f7b5b8e09f08143167
yuezhang27 <yzhanged@gmail.com>
2023-12-08 13:44:40 -0800

    add unit test for check_level_up

--- Test/test_check_level_up.py
+++ Test/test_check_level_up.py
@@ -1,17 +1,37 @@
 """
-Add a docstring
+Unit Test
+check_level_up
 """
 from unittest import TestCase
 
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
+from Main.draft import check_level_up
 
 
 class TestCheckLevelUp(TestCase):
-    def test_check_level_up(self):
-        self.fail()
+    def test_leveled_up(self):
+        character = {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Sanity': 10, 'Experience': 10,
+                     'Darkness': 4, 'Level': 2}
+        expected = True
+        actual = check_level_up(character)
+        self.assertEqual(expected, actual)
+
+    def test_not_level_up_low_experience(self):
+        character = {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Sanity': 10, 'Experience': 9,
+                     'Darkness': 4, 'Level': 1}
+        expected = False
+        actual = check_level_up(character)
+        self.assertEqual(expected, actual)
+
+    def test_not_level_up_highest_level(self):
+        character = {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Sanity': 10, 'Experience': 10,
+                     'Darkness': 4, 'Level': 3}
+        expected = False
+        actual = check_level_up(character)
+        self.assertEqual(expected, actual)
+
+    def test_not_level_up_both(self):
+        character = {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10, 'Sanity': 10, 'Experience': 9,
+                     'Darkness': 4, 'Level': 3}
+        expected = False
+        actual = check_level_up(character)
+        self.assertEqual(expected, actual)
