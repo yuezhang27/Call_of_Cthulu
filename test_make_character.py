2ea3a7e6e153a1bfda26da3115be02ec2ef98d7d
yuezhang27 <yzhanged@gmail.com>
2023-12-08 12:24:17 -0800

    add test of make_character

---
+++ test_make_character.py
@@ -0,0 +1,15 @@
+"""
+Unit Test:
+make_character
+"""
+from unittest import TestCase
+
+from draft import make_character
+
+
+class TestMakeCharacter(TestCase):
+    def test_make_character(self):
+        expected = {'X-coordinate': 0, 'Y-coordinate': 0, 'Current HP': 10,
+                    'Sanity': 10, 'Experience': 0, 'Darkness': 4, 'Level': 1}
+        actual = make_character()
+        self.assertEqual(expected, actual)
